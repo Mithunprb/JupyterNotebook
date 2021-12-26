cf904d8c09d6dc484085c4023cd49642d2999db1
Mithunprb <61266869+Mithunprb@users.noreply.github.com>
2021-12-26 17:14:07 +0530

    initial commit

---
+++ example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define ExecFast ios_base::sync_with_stdio(false),cin.tie(nullptr), cout.tie(nullptr);
+using namespace std;
+ 
+int main(){
+	ExecFast
+	int w;
+	w = 4;
+	(w>2 && w % 2 == 0)?(cout<<"YES"<<"\n"):(cout<<"NO"<<"\n");
+	}
